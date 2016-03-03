c2a7ff6073fce30fcc72c21ba7b754f7954e54e3
geff <zomg21@hotmail.com>
2016-03-03 17:21:20 +0000

    cleaned up con_vector and con_queue functions

--- projv2/con_queue.h
+++ projv2/con_queue.h
@@ -9,40 +9,53 @@ template<class T>
 class con_queue{
 public:
 	//pops the first item off the queue and returns it
-	T pop(){
-		T t;
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		t = queue.front();
-		queue.pop();
-		lock.clear(std::memory_order_seq_cst);
-		return t;
-	}
+	T pop();
 	//pushes the item onto the back of the queue
-	void push(T t){
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		queue.push(t);
-		lock.clear(std::memory_order_seq_cst);
-	}
+	void push(T t);
 	//returns the size of the queue
-	int size(){
-		int a;
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		a = queue.size();
-		lock.clear(std::memory_order_seq_cst);
-		return a;
-	}
-
+	int size();
 	//returns true if the queue is empty
-	bool empty(){
-		bool flag;
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		flag = queue.empty();
-		lock.clear(std::memory_order_seq_cst);
-		return flag;
-	}
+	bool empty();
 private:
 	std::queue<T> queue;
 	std::atomic_flag lock;
 };
+
+template<class T>
+T con_queue<T>::pop(){
+	T t;
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	t = queue.front();
+	queue.pop();
+	lock.clear(std::memory_order_seq_cst);
+	return t;
+}
+
+template<class T>
+void con_queue<T>::push(T t){
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	queue.push(t);
+	lock.clear(std::memory_order_seq_cst);
+}
+
+template<class T>
+int con_queue<T>::size(){
+	int a;
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	a = queue.size();
+	lock.clear(std::memory_order_seq_cst);
+	return a;
+}
+
+template<class T>
+bool con_queue<T>::empty(){
+	bool flag;
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	flag = queue.empty();
+	lock.clear(std::memory_order_seq_cst);
+	return flag;
+}
+
+
 
 #endif
--- projv2/con_vector.h
+++ projv2/con_vector.h
@@ -9,61 +9,82 @@ template<class T>
 class con_vector{
 public:
 	//pushes item to back of the vector
-	void push_back(T t){
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		list.push_back(t);
-		lock.clear(std::memory_order_seq_cst);
-	}
+	void push_back(T t);
 
 	//returns the size of the vector
-	unsigned int size(){
-		unsigned int a;
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		a = list.size();
-		lock.clear(std::memory_order_seq_cst);
-		return a;
-	}
+	unsigned int size();
 
 	//returns true if the vector is empty
-	bool empty(){
-		bool flag;
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		flag = list.empty();
-		lock.clear(std::memory_order_seq_cst);
-		return flag;
-	}
+	bool empty();
 
 	//erases at the index
-	void erase(int index){
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		list.erase(list.begin()+index);
-		lock.clear(std::memory_order_seq_cst);
-	}
+	void erase(int index);
 
 	//returns the item at index
-	T at(int index){
-		T t;
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		t = list.at(index);
-		lock.clear(std::memory_order_seq_cst);
-		return t;
-	}
+	T at(int index);
 
-	//operator overloading for at()
-	T operator[](int index){
-		return at(index);
-	}
+	//operator overloading for element access
+	T operator[](int index);
 
 	//clears the list
-	void clear(){
-		while (lock.test_and_set(std::memory_order_seq_cst));
-		list.clear();
-		lock.clear(std::memory_order_seq_cst);
-	}
+	void clear();
 
 private:
 	std::vector<T> list;
 	std::atomic_flag lock;
 };
+
+template<class T>
+unsigned int con_vector<T>::size(){
+	unsigned int a;
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	a = list.size();
+	lock.clear(std::memory_order_seq_cst);
+	return a;
+}
+
+template<class T>
+void con_vector<T>::push_back(T t){
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	list.push_back(t);
+	lock.clear(std::memory_order_seq_cst);
+}
+
+template<class T>
+bool con_vector<T>::empty(){
+	bool flag;
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	flag = list.empty();
+	lock.clear(std::memory_order_seq_cst);
+	return flag;
+}
+
+template<class T>
+void con_vector<T>::erase(int index){
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	list.erase(list.begin() + index);
+	lock.clear(std::memory_order_seq_cst);
+}
+
+template<class T>
+T con_vector<T>::at(int index){
+	T t;
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	t = list.at(index);
+	lock.clear(std::memory_order_seq_cst);
+	return t;
+}
+
+template<class T>
+T con_vector<T>::operator[](int index){
+	return at(index);
+}
+
+template <class T>
+void con_vector<T>::clear(){
+	while (lock.test_and_set(std::memory_order_seq_cst));
+	list.clear();
+	lock.clear(std::memory_order_seq_cst);
+}
 
 #endif
